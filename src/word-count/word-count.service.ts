f7d3b9653144b8e098c5a311ed8b22824726e7b2
burdeamit@gmail.com <burdeamit@gmail.com>
2023-01-12 17:56:02 +0530

    typecasting

--- src/word-count/word-count.service.ts
+++ src/word-count/word-count.service.ts
@@ -1,44 +1,51 @@
-import { Injectable } from '@nestjs/common';
-import * as fs from 'fs';
+import { Injectable } from "@nestjs/common";
+import * as fs from "fs";
 
 export interface WordFileStats {
-  'All Words': string[];
-  'Total Characters Count': number;
-  'Total Words Count': number;
-  'Words With More Than Seven Characters': string[];
-  'Words With Less Than or With Seven Characters': string[];
+  "All Words": string[];
+  "Total Characters Count": number;
+  "Total Words Count": number;
+  "Words With More Than Seven Characters": string[];
+  "Words With Less Than or With Seven Characters": string[];
 }
 
 @Injectable()
 export class WordCountService {
   async getFileStats(): Promise<WordFileStats> {
     // Read From the file
-    const data = fs.readFileSync('./src/Data/words.txt', 'utf8');
+    const data: string = fs.readFileSync("./src/Data/words.txt", "utf8");
 
     // Read from the file and split the words by space, line break, tab and store in an array
-    const words = data.split(/[\s\t\r]/g);
+    const words: string[] = data.split(/[\s\t\r]/g);
 
     // Filter the words which are length less equal than 7 characters
-    const smallWords = words.filter((word) => word.length <= 7);
+    const smallWords: string[] = words.filter(
+      (word: string): boolean => word.length <= 7
+    );
 
     // Filter the words which are length greater than 7 characters
-    const bigWords = words.filter((word) => word.length > 7);
+    const bigWords: string[] = words.filter(
+      (word: string): boolean => word.length > 77
+    );
 
     //Count the total characters
-    const totalCharacters = words.reduce((acc, word) => {
-      return acc + word.length;
-    }, 0);
+    const totalCharacters: number = words.reduce(
+      (acc: number, word: string): number => {
+        return acc + word.length;
+      },
+      0
+    );
 
     //Count the total words
-    const totalWords = words.length;
+    const totalWords: number = words.length;
 
     // Return the Words which have more than 7 characters
     const response = {
-      'All Words': words,
-      'Total Characters Count': totalCharacters,
-      'Total Words Count': totalWords,
-      'Words With More Than Seven Characters': bigWords,
-      'Words With Less Than or With Seven Characters': smallWords,
+      "All Words": words,
+      "Total Characters Count": totalCharacters,
+      "Total Words Count": totalWords,
+      "Words With More Than Seven Characters": bigWords,
+      "Words With Less Than or With Seven Characters": smallWords,
     };
     return response;
   }
